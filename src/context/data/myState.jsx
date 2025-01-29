8f16b287321ed69a95e908f2c768a8a309ab1c0a
Priyanshu Kumar <priyanshu07264@gmail.com>
2025-01-30 04:14:59 +0530

    Update App.jsx

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import MyContext from './myContext';
-import { fireDb } from '../../firebase/firebaseConfig';
+import React, { useEffect, useState } from 'react';
+import MyContext from "./myContext";
+import { fireDb } from '../../firebase/FirebaseConfig';
 import { Timestamp, addDoc, collection, deleteDoc, doc, getDocs, onSnapshot, orderBy, query, setDoc } from 'firebase/firestore';
 import { toast } from 'react-toastify';
-
 
 function MyState(props) {
   const [mode, setMode] = useState('light');
@@ -13,12 +12,11 @@ function MyState(props) {
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = 'rgb(17, 24, 39)';
-    }
-    else {
+    } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
     }
-  }
+  };
 
   const [products, setProducts] = useState({
     title: null,
@@ -27,139 +25,149 @@ function MyState(props) {
     category: null,
     description: null,
     time: Timestamp.now(),
-    date: new Date().toLocaleString(
-      "en-US",
-      {
+    date: new Date().toLocaleString("en-US", {
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    }),
+  });
+
+  // Add Product Section
+  const addProduct = async () => {
+    if (products.title == null || products.price == null || products.imageUrl == null || products.category == null || products.description == null) {
+      return toast.error('Please fill all fields');
+    }
+    const productRef = collection(fireDb, 'products');
+    setLoading(true);
+    try {
+      await addDoc(productRef, products);
+      toast.success('Product added successfully');
+      getProductData();
+      closeModal();
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+    setProducts({
+      title: null,
+      price: null,
+      imageUrl: null,
+      category: null,
+      description: null,
+      time: Timestamp.now(),
+      date: new Date().toLocaleString("en-US", {
         month: "short",
         day: "2-digit",
         year: "numeric",
-      }
-    )
-
-  })
-
-  // ********************** Add Product Section  **********************
-  const addProduct = async () => {
-    if (products.title == null || products.price == null || products.imageUrl == null || products.category == null || products.description == null) {
-      return toast.error('Please fill all fields')
-    }
-    const productRef = collection(fireDb, "products")
-    setLoading(true)
-    try {
-      await addDoc(productRef, products)
-      toast.success("Product Add successfully")
-      getProductData()
-      closeModal()
-      setLoading(false)
-    } catch (error) {
-      console.log(error)
-      setLoading(false)
-    }
-    setProducts("")
-  }
+      }),
+    });
+  };
 
   const [product, setProduct] = useState([]);
 
-  // ****** get product
+  // Get Product Data
   const getProductData = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
       const q = query(
         collection(fireDb, "products"),
-        orderBy("time"),
-        // limit(5)
+        orderBy("time")
       );
-      const data = onSnapshot(q, (QuerySnapshot) => {
+      const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
         let productsArray = [];
         QuerySnapshot.forEach((doc) => {
           productsArray.push({ ...doc.data(), id: doc.id });
         });
-        setProduct(productsArray)
+        setProduct(productsArray);
         setLoading(false);
       });
-      return () => data;
+      return () => unsubscribe();
     } catch (error) {
-      console.log(error)
-      setLoading(false)
+      console.log(error);
+      setLoading(false);
     }
-  }
-
+  };
 
   const edithandle = (item) => {
-    setProducts(item)
-  }
-  // update product
-  const updateProduct = async (item) => {
-    setLoading(true)
+    setProducts(item);
+  };
+
+  // Update Product
+  const updateProduct = async () => {
+    setLoading(true);
     try {
       await setDoc(doc(fireDb, "products", products.id), products);
-      toast.success("Product Updated successfully")
+      toast.success('Product updated successfully');
       getProductData();
-      setLoading(false)
-      window.location.href = '/dashboard'
+      setLoading(false);
+      window.location.href = '/dashboard';
     } catch (error) {
-      setLoading(false)
-      console.log(error)
+      setLoading(false);
+      console.log(error);
     }
-    setProducts("")
-  }
+    setProducts({
+      title: null,
+      price: null,
+      imageUrl: null,
+      category: null,
+      description: null,
+      time: Timestamp.now(),
+      date: new Date().toLocaleString("en-US", {
+        month: "short",
+        day: "2-digit",
+        year: "numeric",
+      }),
+    });
+  };
 
   const deleteProduct = async (item) => {
-
     try {
-      setLoading(true)
+      setLoading(true);
       await deleteDoc(doc(fireDb, "products", item.id));
-      toast.success('Product Deleted successfully')
-      setLoading(false)
-      getProductData()
+      toast.success('Product deleted successfully');
+      setLoading(false);
+      getProductData();
     } catch (error) {
-      // toast.success('Product Deleted Falied')
-      setLoading(false)
+      setLoading(false);
     }
-  }
-
+  };
 
   const [order, setOrder] = useState([]);
 
   const getOrderData = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
-      const result = await getDocs(collection(fireDb, "orders"))
+      const result = await getDocs(collection(fireDb, "orders"));
       const ordersArray = [];
       result.forEach((doc) => {
         ordersArray.push(doc.data());
-        setLoading(false)
       });
       setOrder(ordersArray);
-      // console.log(ordersArray)
       setLoading(false);
     } catch (error) {
-      console.log(error)
-      setLoading(false)
+      console.log(error);
+      setLoading(false);
     }
-  }
-
+  };
 
   const [user, setUser] = useState([]);
 
   const getUserData = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
-      const result = await getDocs(collection(fireDb, "user"))
+      const result = await getDocs(collection(fireDb, "user"));
       const usersArray = [];
       result.forEach((doc) => {
         usersArray.push(doc.data());
-        setLoading(false)
       });
       setUser(usersArray);
-      console.log(usersArray)
       setLoading(false);
     } catch (error) {
-      console.log(error)
-      setLoading(false)
+      console.log(error);
+      setLoading(false);
     }
-  }
-
+  };
 
   useEffect(() => {
     getProductData();
@@ -167,22 +175,37 @@ function MyState(props) {
     getUserData();
   }, []);
 
-  const [searchkey, setSearchkey] = useState('')
-  const [filterType, setFilterType] = useState('')
-  const [filterPrice, setFilterPrice] = useState('')
-
+  const [searchkey, setSearchkey] = useState('');
+  const [filterType, setFilterType] = useState('');
+  const [filterPrice, setFilterPrice] = useState('');
 
   return (
-    <MyContext.Provider value={{
-      mode, toggleMode, loading, setLoading,
-      products, setProducts, addProduct, product,
-      updateProduct,edithandle,deleteProduct,order,user,
-      searchkey, setSearchkey,filterType, setFilterType,
-      filterPrice, setFilterPrice
-    }}>
+    <MyContext.Provider
+      value={{
+        mode,
+        toggleMode,
+        loading,
+        setLoading,
+        products,
+        setProducts,
+        addProduct,
+        product,
+        updateProduct,
+        edithandle,
+        deleteProduct,
+        order,
+        user,
+        searchkey,
+        setSearchkey,
+        filterType,
+        setFilterType,
+        filterPrice,
+        setFilterPrice,
+      }}
+    >
       {props.children}
     </MyContext.Provider>
-  )
+  );
 }
 
-export default MyState
+export default MyState;
