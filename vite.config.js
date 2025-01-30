c5463d1dce23cae78884d6d4301044dc92d0d168
Priyanshu Kumar <priyanshu07264@gmail.com>
2025-01-30 23:56:45 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/pk_ka_mart/'
+  base: '/pk_ka_mart/', // Make sure this matches your GitHub Pages repository name or deployment path
 })
