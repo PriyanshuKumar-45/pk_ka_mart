6b83afa6ff6f3a4b9743160d154eff2ca311f209
Priyanshu Kumar <priyanshu07264@gmail.com>
2025-01-31 00:28:40 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pk_ka_mart/', // Make sure this matches your GitHub Pages repository name or deployment path
+  base: '/pk_ka_mart/', // Ensure this matches your GitHub Pages repository name or deployment path
+  
+  build: {
+    // Customize chunking to reduce large bundles
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split node_modules into a separate vendor chunk
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    },
+    
+    // Increase the chunk size warning limit if desired
+    chunkSizeWarningLimit: 1000, // Set this to a higher value like 1000 KB (1 MB)
+  }
 })
