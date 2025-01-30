9aef2731dfc68250e4abdccec35a022636515239
Priyanshu Kumar <priyanshu07264@gmail.com>
2025-01-31 01:22:58 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pk_ka_mart/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split vendor libraries into their own chunk
+          if (id.includes('node_modules')) {
+            return 'vendor'; // This will create a chunk called "vendor.js"
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,  // Increase chunk size warning limit to 1MB
+  }
 })
